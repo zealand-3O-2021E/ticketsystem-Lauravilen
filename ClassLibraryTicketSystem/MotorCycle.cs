bba9a79dd9e265709deb45e80ee47601e7a83e6a
Lauravilen <44665204+Lauravilen@users.noreply.github.com>
2021-09-26 17:54:26 +0200

    Added MC-class. Added new test class to test MC. Code coverage 100%. Class documented and tested.

---
+++ ClassLibraryTicketSystem/MotorCycle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTicketSystem
+{
+    public class MotorCycle
+    {
+        /// <summary>
+        /// Gets or sets the license plate of the motorcycle
+        /// </summary>
+        public string LicensePlate { get; set; }
+        /// <summary>
+        /// Gets or sets the date of crossing
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Represents a motorcycle and contains relevant information about the MC, for the ticketing system
+        /// </summary>
+        /// <param name="licensePlate">The MC's license plate</param>
+        /// <param name="date">The date of crossing</param>
+        public MotorCycle(string licensePlate, DateTime date)
+        {
+            LicensePlate = licensePlate;
+            Date = date;
+        }
+
+        /// <summary>
+        /// Returns the ticket price as a double
+        /// </summary>
+        /// <returns>125</returns>
+        public double Price()
+        {
+            return 125;
+        }
+
+        /// <summary>
+        /// Returns the motorcycle's vehicle type
+        /// </summary>
+        /// <returns>MC</returns>
+        public string Vehicle()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ VehicleTests/MCTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryTicketSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VehicleTests
+{
+    [TestClass]
+    public class MCTests
+    {
+        [TestMethod]
+        public void YearMonthDay_DateTest()
+        {
+            MotorCycle  mc = new MotorCycle("VB896621", new DateTime(2021, 08, 03));
+            Assert.AreEqual(new DateTime(2021, 08, 03), mc.Date);
+        }
+
+        [TestMethod]
+        public void LicenseTest()
+        {
+            DateTime date = new DateTime(2021, 03, 19);
+            MotorCycle mc = new MotorCycle("VB896621", date);
+
+            Assert.AreEqual("VB896621", mc.LicensePlate);
+        }
+
+        [TestMethod]
+        public void PriceTest()
+        {
+            DateTime date = new DateTime(2021, 03, 19);
+            MotorCycle mc = new MotorCycle("VB896621", date);
+
+            Assert.AreEqual(125, mc.Price());
+        }
+
+        [TestMethod]
+        public void TypeTest()
+        {
+            DateTime date = new DateTime(2021, 03, 19);
+            MotorCycle mc = new MotorCycle("VB896621", date);
+
+            Assert.AreEqual("MC", mc.Vehicle());
+        }
+
+    }
+}
